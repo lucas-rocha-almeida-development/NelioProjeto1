e20400ec84c6cb51d0bf77c76363925127da19b0
Lucas Almeida <lucalmr12@gmail.com>
2023-06-01 15:26:58 -0300

    Adicionar arquivos de projeto.

---
+++ NelioProjeto1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NelioProjeto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome completo:" );
+            string nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            int quartos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com preço de um produto: ");
+            double Preco = double.Parse(Console.ReadLine());
+            Console.WriteLine("Coloque seu nome,idade e altura:");
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
